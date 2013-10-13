3a672c2f0c444c4f76561dc4b11f2a7b607a67e6
jedi <jedi2454@gmail.com>
2013-10-13 23:09:24 +0800

    tonight

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Demo::Application.routes.draw do
+  resources :people
+
+  get "welcome/say_hello" => "welcome#say"
+  get "welcome" => "welcome#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -39,7 +43,7 @@ Demo::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
